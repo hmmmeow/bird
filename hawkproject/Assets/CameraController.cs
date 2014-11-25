121d389f355094615c2decfcda9e8df3ebfd83a7
FelixMeow <fm_matrix@hotmail.com>
2014-11-25 21:57:08 +0000

    new folder

---
+++ hawkproject/Assets/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform character;
+	private Vector3 moveTemp;
+	float camspeed = 3;
+	float xDifference;
+	float yDifference;
+	float movementThreshold = 1;
+
+	
+	void Update () 
+	{
+
+
+
+
+		if (character.transform.position.x > transform.position.x)
+				{
+			xDifference = character.transform.position.x - transform.position.x;
+				}
+		else
+				{
+			xDifference = transform.position.x - character.transform.position.x;
+				}
+
+
+		if (character.transform.position.y > transform.position.y)
+		{
+			yDifference = character.transform.position.y - transform.position.y;
+		}
+		else
+		{
+			yDifference = transform.position.y - character.transform.position.y;
+		}
+
+
+		if (xDifference >= movementThreshold || yDifference >= movementThreshold)
+			{
+			moveTemp = character.transform.position;
+			moveTemp.z = -1;
+			transform.position = Vector3.MoveTowards (transform.position, moveTemp, camspeed * Time.deltaTime);
+						}
+
+	
+	}
+
+
+
+}
+
+
---
+++ hawkproject/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate()
+	{
+		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward);
+		transform.rotation = rot;
+
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		rigidbody2D.angularVelocity = 0;
+
+		if (Input.GetKey (KeyCode.W)) {
+						rigidbody2D.AddForce (gameObject.transform.up * speed);
+				}
+			
+	
+	}
+}
