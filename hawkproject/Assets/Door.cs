18dacfbac63c34dc998c8094dd946f6ec8a7c1a7
FelixMeow <fm_matrix@hotmail.com>
2014-11-26 10:20:04 +0000

    Fail files

--- hawkproject/Assets/Door.cs
+++ hawkproject/Assets/Door.cs
@@ -3,27 +3,13 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 
-	public Transform doorTrigger;
-	public Transform doorObject;
-
-	void OnTriggerEnter2D(Collider2D other) 
-	{
-		Invoke ("DoorOpen", 0f);
+	// Use this for initialization
+	void Start () {
+	
 	}
 	
-	void DoorOpen()
-	{
-		doorObject.transform.position = new Vector3 (doorObject.position.x - 3, doorObject.position.y, doorObject.position.z);
-		doorTrigger.transform.position = new Vector3 (doorTrigger.position.x - 20, doorTrigger.position.y, doorTrigger.position.z);
-		Invoke ("DoorClose", 3f);
-	}
+	// Update is called once per frame
+	void Update () {
 	
-	void DoorClose()
-	{
-		doorObject.transform.position = new Vector3 (doorObject.position.x + 3, doorObject.position.y, doorObject.position.z);
-		doorTrigger.transform.position = new Vector3 (doorTrigger.position.x + 20, doorTrigger.position.y, doorTrigger.position.z);
 	}
-
-
-
 }
--- hawkproject/Assets/PlayerController.cs
+++ hawkproject/Assets/PlayerController.cs
@@ -5,59 +5,28 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 
-
 	void FixedUpdate()
 	{
 		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward);
 		transform.rotation = rot;
 
-		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z-180);
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
 		rigidbody2D.angularVelocity = 0;
 
-		//if (Input.GetKey (KeyCode.S)) {
-		//				rigidbody2D.AddForce (gameObject.transform.up * speed);
-		//		}
-		//if (Input.GetKey (KeyCode.W)) {
-		//	rigidbody2D.AddForce (-gameObject.transform.up * speed);
-		//}
-
-		//if (Input.GetKey (KeyCode.W)) {
-		//	rigidbody2D.AddForce (gameObject.transform.up);
-		//}
-
-		//if (Input.GetKey (KeyCode.A)) {
-		//	rigidbody2D.AddForce
-		//	rigidbody2D.AddForceAtPosition (gameObject.transform.up, gameObject.transform.position.z+1);
-		//}
-
-
-		//if (Input.GetKey (KeyCode.D)) {
-		//	player.transform.position = new Vector3 (player.transform.position.x + speed, player.transform.position.y, player.transform.position.z);
-		//}
-
-		//if (Input.GetKey (KeyCode.A)) {
-		//	player.transform.position = new Vector3 (player.transform.position.x - speed, player.transform.position.y, player.transform.position.z);
-		//}
-
-		//if (Input.GetKey (KeyCode.W)) {
-		//	player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + speed, player.transform.position.z);
-		//}
-
-		//if (Input.GetKey (KeyCode.S)) {
-		//	player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - speed, player.transform.position.z);
-		//}
+		if (Input.GetKey (KeyCode.W)) {
+						rigidbody2D.AddForce (gameObject.transform.up * speed);
+				}
 			
 	
 	}
 
-	void Update()
+
+	void OnTriggerEnter2D(Collider2D other) 
 	{
-		transform.Translate(-Vector2.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed, Space.Self);
-		transform.Translate(-Vector2.up * Input.GetAxis("Vertical") * Time.deltaTime * speed, Space.Self);
+		Destroy (other.gameObject);
+
 	}
-
-
 
 
 }
