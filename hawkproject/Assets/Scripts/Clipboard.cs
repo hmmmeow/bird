5f2428830f129a2519f0efba325ccce0e4740ddd
FelixMeow <fm_matrix@hotmail.com>
2014-11-26 23:40:36 +0000

    Clipboard fail

---
+++ hawkproject/Assets/Scripts/Clipboard.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Clipboard : MonoBehaviour {
+
+	//public Transform bedArea;
+
+	//Canvas clipboard;
+	
+	//void Start()
+	//{
+	//	clipboard = GetComponent<Canvas>();
+	//	clipboard.enabled = false;
+	//}
+
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		Invoke ("ClipboardToggle", 0f);
+	}
+
+
+
+	public void ClipboardToggle()
+	{
+
+		//if (Input.GetKeyDown(KeyCode.Tab))
+		//{
+		print ("works");	
+		//clipboard.enabled = !clipboard.enabled;
+		//}
+
+		//clipboard.enabled = !clipboard.enabled;
+		//Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+	}
+
+
+
+}
