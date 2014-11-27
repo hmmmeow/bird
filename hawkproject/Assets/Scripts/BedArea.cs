1da7e955ca9ed143d3955e1568a9dea2040e4ba4
FelixMeow <fm_matrix@hotmail.com>
2014-11-27 03:40:47 +0000

    2 patients half working

--- hawkproject/Assets/Scripts/BedArea.cs
+++ hawkproject/Assets/Scripts/BedArea.cs
@@ -1,16 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 
 public class BedArea : MonoBehaviour {
 
-
 	public Clipboard clipboard;
+	public static string bedspace;
+	Text text;
+
+	//public Text newtext;
+
+	//public string clipboardtext;
+	//Text currenttext;
+	//void Awake()
+	//	{
+	//	text = GetComponent<Text>();
+	//	}
+	//void Update (){
+	//	clipboardtext = currenttext.text;
+	//	print (clipboardtext);
+	//	}
+
 
 	void OnTriggerStay2D(Collider2D other)
 	{
 		Invoke ("ShowClipboardNow", 0f);
+		text = GetComponent<Text> ();
+		bedspace = text.text;
+	//	Invoke ("NewText", 0f);
 	}
+
+
+	//public void NewText()
+	//{
+	//	newtext = GetComponent<Text>();
+	//}
+
 
 	void ShowClipboardNow()
 	{
@@ -19,6 +45,8 @@ public class BedArea : MonoBehaviour {
 			clipboard.ClipboardToggle();
 		}
 	}
+
+
 
 
 }
---
+++ hawkproject/Assets/Scripts/RetrieveClipboardText.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RetrieveClipboardText : MonoBehaviour {
+
+	public BedArea bedarea;
+
+	Text clipboardtext;
+	Text currentclipboard;
+	public string bedspace = "1";
+
+
+	void Awake()
+	{
+		clipboardtext = GetComponent<Text>();
+		currentclipboard = bedarea.GetComponent<Text> ();
+		bedspace = currentclipboard.text;
+	}
+	
+
+	void Update (){
+
+		if (bedspace == "1")
+		{
+			clipboardtext.text = "Patient 1";
+		}
+
+		if (bedspace == "2")
+		{
+			clipboardtext.text = "Patient 2";
+		}
+
+
+		//clipboardtext.text = bedarea.newtext.text;
+		
+	}
+
+}
