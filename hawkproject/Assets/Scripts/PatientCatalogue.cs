a4d96fdc22142cb80fae4349a5ef8f49b04354ff
FelixMeow <fm_matrix@hotmail.com>
2014-12-02 19:50:34 +0000

    filenames make sense

---
+++ hawkproject/Assets/Scripts/PatientCatalogue.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatientCatalogue : MonoBehaviour {
+
+	string firstName;
+	string lastName;
+	string fullName;
+	int getAge;
+	string diagnosisName;
+	string associatedSymptoms;
+
+	string getRandomFirstName(){
+		string[] patientFName = {"Harry","John","Will","Thomas","Charlie","Felix","Dave","Mike","Steven","Ben","Rob","Matthew","Dennis","Brett","Joe","Justin","Lenny","Gavin"};
+		int randomiser = Random.Range (0, patientFName.Length);
+		firstName = patientFName[randomiser];
+		return firstName;
+	}
+
+	string getRandomLastName(){
+		string[] patientLName = {"Hobart","Smith","Dickinson","Cope","Gilder","Seddon","Swann","Dixon","Gellar","Green","Wright","Jackson","Moyes","Harris","Petit","Morgan","Biddle","Tank","Lewis","Robinson"};
+		int randomiser = Random.Range (0, patientLName.Length);
+		lastName = patientLName[randomiser];
+		return lastName;
+	}
+
+	public string getRandomFullName()
+	{
+		string firstName = getRandomFirstName();
+		string lastName = getRandomLastName();
+		fullName = firstName + " " + lastName;
+		return fullName;
+		
+	}
+
+	public int getRandomAge(){
+		getAge = Random.Range (26, 79);
+		return getAge;
+	}
+
+	public string getRandomDiagnosis(){
+		string[] patientDiagnosis = {"Squits","Hairyitis","Dendritic rash","Crumblies"};
+		int randomiser = Random.Range (0, patientDiagnosis.Length);
+		diagnosisName = patientDiagnosis[randomiser];
+		return diagnosisName;
+	}
+
+	// randomiser ends here
+
+//	void symptomsFromDiagnosis(){
+//
+//
+//		if  (diagnosisName == null) {
+//			associatedSymptoms = "D'oh!";
+//		}
+//
+//		// symptoms based on random diagnosis - are these classes now?
+//
+//		if  (diagnosisName == "Squits") {
+//			associatedSymptoms = "Diarrhoea";
+//		}
+//		if  (diagnosisName == "Hairyitis") {
+//			associatedSymptoms = "Itchy";
+//		}
+//		if  (diagnosisName == "Dendritic rash") {
+//			associatedSymptoms = "Itchy";
+//		}
+//		if  (diagnosisName == "Crumblies") {
+//			associatedSymptoms = "Back pain";
+//		}
+//
+//	}
+
+//	public string getAssociatedSymptoms(){
+//
+//		symptomsFromDiagnosis ();
+//		return associatedSymptoms;
+//				
+//	
+//	}
+
+
+
+}
+
+
--- hawkproject/Assets/Scripts/PatientDetails.cs
+++ hawkproject/Assets/Scripts/PatientDetails.cs
@@ -3,37 +3,39 @@ using System.Collections;
 
 public class PatientDetails : MonoBehaviour {
 
-	public Patient patient;
-	string firstName;
-	string lastName;
-	string fullName;
-	int age;
+
 
 //	Diagnosis<List> diagnosis;
 //	DrugAllergies drugAllergies;
 
-	public string patientFullName()
-	{
-		firstName = patient.getRandomFirstName();
-		lastName = patient.getRandomLastName();
-		fullName = firstName + " " + lastName;
-		return fullName;
-				
-	}
-
-	public int patientAge()
-	{
-		age = patient.getRandomAge();
-		return age;
-		
-	}
-
-//	void Start(){
-//		patientFullName ();
-//		patientAge ();
-//		/print (fullName + " is " + age + " years old");
+//	public string patientFullName()
+//	{
+//		string firstName = patient.getRandomFirstName();
+//		string lastName = patient.getRandomLastName();
+//		string fullName = firstName + " " + lastName;
+//		return fullName;
+//				
+//	}
+//
+//	public int patientAge()
+//	{
+//		int age = patient.getRandomAge();
+//		return age;
+//		
+//	}
+//
+//	public string patientDiagnosis()
+//	{
+//		string theDiagnosis = patient.getDiagnosis();
+//		return theDiagnosis;
 //		
 //	}
 
-		
+
+//	public string patientSymptoms()
+//	{
+//		if (patientDiagnosis()
+//		
+//		
+//	}
 }
